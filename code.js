03840b47af11a2e0ea2d26e8af09e90c17946d6a
Teodor Doroftei <teodor.doroftei@outlook.com>
2018-03-13 10:24:05 +0100

    added star ratings with js

--- code.js
+++ code.js
@@ -9,4 +9,23 @@ function renderMovie(movieData){
     }
     document.getElementById("actors").innerHTML=actorList;
 }
+
+function changeStarRating(rating){
+    for(let i=1; i<=5; i++){
+      let star = document.getElementById("star" + i);
+      if (i <= rating){
+        star.classList.add("filled");
+      } else {
+        star.classList.remove("filled");
+      }
+    }
+  }
+  
+  for(let i=1; i<=5; i++){
+    let star = document.getElementById("star" + i);
+    star.addEventListener("click", function(){
+      changeStarRating(i);
+    });
+  }
+
 renderMovie(movieData)

575459559dff1cd3356965274391e22e297885b6
Teodor Doroftei <teodor.doroftei@outlook.com>
2018-03-13 11:59:47 +0100

    changed to use jQuery

--- code.js
+++ code.js
@@ -1,30 +1,40 @@
 function renderMovie(movieData){
-    document.getElementById("image").setAttribute("src", movieData.image);
-    document.getElementById("movietitle").innerText=movieData.title;
-    document.getElementById("short").innerText=movieData.short;
+    $("#image").attr("src", movieData.image);
+    $("#movietitle").text(movieData.title);
+    $("#short").text(movieData.short);
 
     let actorList="";
     for(let i=0; i<movieData.actors.length; i++){
         actorList+="<li>"+movieData.actors[i]+"</li>";
     }
-    document.getElementById("actors").innerHTML=actorList;
+    $("#actors").html(actorList);
 }
 
 function changeStarRating(rating){
     for(let i=1; i<=5; i++){
-      let star = document.getElementById("star" + i);
+      let star = $("#star" + i);
       if (i <= rating){
-        star.classList.add("filled");
+        star.addClass("filled");
       } else {
-        star.classList.remove("filled");
+        star.removeClass("filled");
       }
     }
   }
   
+  //-------------------------------------------------------
+let currentRating;
+
   for(let i=1; i<=5; i++){
-    let star = document.getElementById("star" + i);
-    star.addEventListener("click", function(){
+    let star = $("#star" + i);
+    star.on("click", function(){
+        currentRating=i;
       changeStarRating(i);
+    });
+    star.on("mouseover", function(){
+        changeStarRating(i);
+    });
+    star.on("mouseout", function(){
+        changeStarRating(currentRating);
     });
   }
 

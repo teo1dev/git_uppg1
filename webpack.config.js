a8c6e8ee28d95e65ad9a0149dfd517c7ad0728b2
Teodor Doroftei <teodor.doroftei@outlook.com>
2018-03-14 10:12:11 +0100

    moved js files to source dir

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports={
     mode: 'development', 
-    entry: './code.js',
+    entry: './source/code.js',
     output:{
         path: path.resolve(__dirname, "dist"),
         filename: 'bundle.js'
